e983f1c0ab0301a4b243d08fdac147470dda8c4b
Alvaro <alvarohernandezrad@gmail.com>
2020-10-11 19:46:07 +0200

    erabailtzaile pasahitza kautotu

--- src/main/java/ehu/isad/controllers/Controller.java
+++ src/main/java/ehu/isad/controllers/Controller.java
@@ -1,4 +1,30 @@
 package ehu.isad.controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
+
+    @FXML
+    private TextField txtErabiltzailea;
+
+    @FXML
+    private TextField txtPasahitza;
+
+    @FXML
+    private Label lblErabiltzailea;
+
+    @FXML
+    private Label lblPasahitza;
+
+    @FXML
+    private Button btnKautotu;
+
+    @FXML
+    void onClick(ActionEvent event) {
+        System.out.println(txtErabiltzailea.getText()+":"+txtPasahitza.getText());
+    }
+
 }
